cfea90569ec367890115e51bd3652f624271f20f
LeonardoMantovani <leonardo@mantovani.us>
2020-09-13 19:57:46 +0200

    Created project's main structure

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:coffee_crew/screens/authenticate/authenticate.dart';
+import 'package:coffee_crew/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return home or authenticate widget
+    return Authenticate();
+  }
+}
